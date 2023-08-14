4cadd733b3864109edfb5aca92698e920e3e7596
Magdalena Trzcińska <s22536@pjwstk.edu.pl>
2023-08-14 19:06:06 +0200

    add the serivce

---
+++ Data/TopicService.cs
@@ -0,0 +1,45 @@
+﻿using EmailNewsletter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailNewsletter.Data
+{
+    public interface ITopicService
+    {
+        Task<ICollection<Topic>> GetTopicsAsync();
+        Task<int> GetCountTopicsAsync();
+        Task AddNewTopic(Topic topic);
+        Task RemoveTopic(Topic topic);
+    }
+
+    public class TopicService : ITopicService
+    {
+        private readonly NewsletterContext _newsletterContext;
+
+        public TopicService(NewsletterContext newsletterContext)
+        {
+            _newsletterContext = newsletterContext;
+        }
+
+        public async Task<ICollection<Topic>> GetTopicsAsync()
+        {
+            return await _newsletterContext.Topics.ToListAsync();
+        }
+
+        public async Task<int> GetCountTopicsAsync()
+        {
+            return await _newsletterContext.Topics.CountAsync();
+        }
+
+        public async Task AddNewTopic(Topic topic)
+        {
+            _newsletterContext.Topics.Add(topic);
+            await _newsletterContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveTopic(Topic topic)
+        {
+            _newsletterContext.Topics.Remove(topic);
+            await _newsletterContext.SaveChangesAsync();
+        }
+    }
+}
