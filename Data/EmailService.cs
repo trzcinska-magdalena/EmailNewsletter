cf2ca2c3de6ec099f94c25eb4f6a55442874fe4b
Magdalena Trzcińska <s22536@pjwstk.edu.pl>
2023-08-15 18:49:05 +0200

    add the email service

---
+++ Data/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using EmailNewsletter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailNewsletter.Data
+{
+    public interface IEmailService
+    {
+        Task<ICollection<Email>> GetAllEmails();
+    }
+
+    public class EmailService : IEmailService
+    {
+        private readonly NewsletterContext _newsletterContext;
+
+        public EmailService(NewsletterContext newsletterContext)
+        {
+            _newsletterContext = newsletterContext;
+        }
+
+        public async Task<ICollection<Email>> GetAllEmails()
+        {
+            try
+            {
+                return await _newsletterContext.Emails.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                //TODO
+                throw new Exception();
+            }
+        }
+    }
+}
